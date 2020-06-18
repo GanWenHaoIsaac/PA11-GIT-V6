1b7a44390f50986963e75fc0ff86a374d5e61b81
GanWenHaoIsaac <isaacgan64@gmail.com>
2020-06-18 18:56:52 +0800

    Implemented the new features

---
+++ 32_PA11_GanWenHaoIsaac/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager thisManager = null;
+    [SerializeField] private Text Txt_Score = null;
+    private int Score = 0;  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore(int value)
+    {
+        Score++;
+        Txt_Score.text = "Score : " + Score;
+    }
+}
